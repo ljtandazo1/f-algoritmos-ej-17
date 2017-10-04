7d50bc7963cd231c723c584571b535bd20ecf600
Lisber <lismonta1996@gmail.com>
2017-10-04 11:57:11 -0500

    Nuevo Proyecto

---
+++ Clase1/Proyecto1/src/proyecto1/Proyecto1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Salas
+ */
+public class Proyecto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        // TODO code application logic here
+        System.out.println("Ingrese un Numero");
+        
+        
+    }
+    
+}
